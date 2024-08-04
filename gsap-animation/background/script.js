87351e350a694dacd5a8f733081d237b3b97b03c
Nomeena <nominaabbasis@gmail.com>
2024-08-04 23:34:03 +0500

    add  gsap

--- gsap-animation/background/script.js
+++ gsap-animation/background/script.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     $scramble.scramble(3000, 20, "alphabet" , true)
 });
 
-// ye animation dealy krne k liye likha gya hy agar ye nhi likhen gy t animation foran se hojaega
+//ye animation dealy krne k liye likha gya hy agar ye nhi likhen gy t animation foran se hojaega
 var tl = gsap.timeline();
 tl.to(".loading" ,{
     opacity: 0,
@@ -20,4 +20,4 @@ tl.to(".loading" ,{
 .to(".loader",{
     y:"-100%",
     duration : 1,
-})
+}, "up")
