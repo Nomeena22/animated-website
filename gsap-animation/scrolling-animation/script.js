dafc789ac8e23345722b1f91b716e6a0efa1118b
Nomeena <nominaabbasis@gmail.com>
2024-08-05 17:12:13 +0500

    js

---
+++ gsap-animation/scrolling-animation/script.js
@@ -0,0 +1,13 @@
+var tl = gsap.timeline({scrollTrigger :{
+    trigger : "main",
+    markers : true ,
+    start: "75% 50%", 
+    end :"100% 50%",
+    scrub : 3,
+    duration : 1,
+    pin : true,
+}})
+.to (".img",{
+    width : "100%",
+    height : "100vh",
+})
